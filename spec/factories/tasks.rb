142001f99d1a46011defb8fb3b71650e32fb5806
naoko kasahara <kassa0918@gmail.com>
2021-02-04 01:39:00 +0900

    日本語化、ローカルタイム、index降順並び替え、降順並び替えテスト実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
     factory :task do
         title { 'title1' }
         content { 'title1_content' }
+        id {1}
       end
       # 作成するテストデータの名前を「second_task」とします
       # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
       factory :second_task, class: Task do
         title { 'title2' }
         content { 'title2_content' }
+        id{2}
       end
 end
