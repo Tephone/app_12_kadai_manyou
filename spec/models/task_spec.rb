fd4043657a251d8b721761ab4f18c988fb07397a
naoko kasahara <kassa0918@gmail.com>
2021-02-07 01:21:58 +0900

    modelテスト追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 describe 'タスクモデル機能', type: :model do
+  before do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    @task1 = FactoryBot.create(:task)
+    @task2 = FactoryBot.create(:second_task)
+  end
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
@@ -25,6 +30,35 @@ describe 'タスクモデル機能', type: :model do
         expect(task).to be_valid
         #expect(page).to have_content 'title2'
         # ここに内容を記載する
+      end
+    end
+  end
+  describe '検索機能' do
+    # 必要に応じて、テストデータの内容を変更して構わない
+    # let!(:task) { FactoryBot.create(:task, title: 'task') }
+    # let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
+        expect(Task.title_search('1')).to include(@task1)
+        expect(Task.title_search('1')).not_to include(@task2)
+        expect(Task.title_search('1').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # ここに内容を記載する
+        expect(Task.status_search('未了')).to include(@task2)
+        expect(Task.status_search('未了')).not_to include(@task1)
+        expect(Task.status_search('未了').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        # ここに内容を記載する
+        expect(Task.status_search('未了').title_search('title')).to include(@task2)
+        expect(Task.status_search('未了').title_search('title')).not_to include(@task1)
+        expect([Task.status_search('未了').title_search('title')].count).to eq 1
       end
     end
   end
