c41e6737cbaa22b77ff9b820204e743de76e0ca1
naoko kasahara <kassa0918@gmail.com>
2021-02-08 12:47:57 +0900

    微修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,9 +4,12 @@ class Task < ApplicationRecord
   enum status: { 未了: 0, 対応中: 1, 完了: 2 }
   enum priority: { 低: 0, 中: 1, 高: 2 }
 
-  scope :title_search, -> (title) { where('title LIKE ?', "%#{title}%")}
-  scope :status_search, -> (status) { where(status: status)}
+  #scope :title_search, -> (title) { where('title LIKE ?', "%#{title}%")} titleは引数（パラメーターで受け取った値がここに入る）
+  scope :status_search, -> (status) { where(status: status)} #statusは引数（パラメーターで受け取った値がここに入る）
   
+  scope :title_search, -> (title) do  #scopeはメゾットで、title_searchは設定したメゾット名
+    where('title LIKE ?', "%#{title}%")
+  end
   # def self.search(search_word)
   #   search_word ? where('title LIKE ?', "%#{search_word}%") : all
   #   #search_word ? where("%#{search_word}%") : all
