3df10071f49184293af69be7ca2f6a681ca256ad
naoko kasahara <kassa0918@gmail.com>
2021-02-10 11:13:48 +0900

    heroku確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,20 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 }
     #enum admin: { 無: false, 有: true }
     before_destroy :not_destroy_by_self
-    #before_update :not_update_by_self
+    before_update :not_destroy_by_self
     private
     def not_destroy_by_self
-        if self.admin && User.where(admin: true).count < 2
+        if self.admin && User.where(admin: true).count == 1
             throw(:abort)
         end
     end
-    def not_update_by_self
-        if self.admin && User.where(admin: true).count < 2
-            if admin == false
-                throw(:abort)
-            end
-        end
-    end
+    # def not_update_by_self
+    #     if self.admin && User.where(admin: true).count < 2
+    #         # if admin == false
+    #         #     throw(:abort)
+    #         # end
+    #     end
+    # end
     # before_destroy :not_destroy_by_self, only: %i[ update destroy ]
  
             
